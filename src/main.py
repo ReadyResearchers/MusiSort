86609efb531aaf6cc25f24e895ba652f8a5d4b92
gvanzin-allegheny <vanzing@allegheny.edu>
2022-09-23 05:50:08 -0400

    Removed unneeded file

--- src/main.py
+++ src/main.py
@@ -1,21 +1,29 @@
 import typer
+import glob
+import os
+import music_tools as mst
+
+# Common audio file extensions to look for
+audio_formats = ["mp3", "ogg", "wav", "flac", "m4a", "aac", "webm", "opus"]
 
 # Returned by load_songs
 songs_list = []
 # Returned by get_wave_data_from_songs
 songs_data = []
 
-def main(path: str):
-    print("path chosen :", path)
+def main():
+    print("The current directory is at :", os.getcwd())
+    path = os.getcwd() + input("What folder should music be read in from? : ")
+    for audio_format in audio_formats:
+        for audio_file in sorted(glob.glob(path + "*." + audio_format)):
+            songs_list.append("" + audio_file)
+    print("\nFound", len(songs_list), "audio files within the folder!")
+    if (input("\nIs this correct? (y/n): ")).lower() == "y":
+        mst.load_audio_from_files(songs_list)
+    else:
+        print("\nGood day.")
+        exit()
     
     
 if __name__ == "__main__":
-    typer.run(main)
-
-#def load_songs(folder_path_to_songs):
-#   songs_list = ["./frog.mp3", "./cat.mp3"];
-    
-#def get_wavedata_from_songs():
-#    for song in songs_list:
-#        array = {200, 3, 20, 71, 23, 8, 1}
-#        songs_data.append((song, array))
+    main()
--- src/music_tools.py
+++ src/music_tools.py
@@ -1,3 +1,23 @@
 import librosa
 import os
 import platform
+import sys
+
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+def load_audio_from_files(paths):
+    for index, path in enumerate(paths):
+        print("Max of", index, ":", read_audio_file(path));
+
+def read_audio_file(path):
+    try:
+        y, sr = librosa.load(path)
+        max = 0
+        for i in y:
+            if(max < abs(i)):
+                max = i
+        return max
+    except Exception as e:
+        print("An error occured in loading an audio file : \n", e)
