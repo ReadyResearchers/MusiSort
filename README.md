b370da8b3a0d65dfbaac452b11f6b34b589210ca
gvanzin-allegheny <vanzing@allegheny.edu>
2022-09-23 05:01:46 -0400

    Add dependency info to readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AutoMusicSort
 A tool used to automatically sort lists of music into similar categories.
+
+Current Dependencies Needed:
+
+- Typer : pip install "typer[all]"
+- Librosa : pip install Librosa
+- NumPy : pip install numpy
+- ffmpeg : pip install ffmpeg
--- src/main.py
+++ src/main.py
@@ -12,10 +12,10 @@ def main(path: str):
 if __name__ == "__main__":
     typer.run(main)
 
-//def load_songs(folder_path_to_songs):
-    songs_list = ["./frog.mp3", "./cat.mp3"];
+#def load_songs(folder_path_to_songs):
+#   songs_list = ["./frog.mp3", "./cat.mp3"];
     
-//def get_wavedata_from_songs():
-    for song in songs_list:
-        array = {200, 3, 20, 71, 23, 8, 1}
-        songs_data.append((song, array))
+#def get_wavedata_from_songs():
+#    for song in songs_list:
+#        array = {200, 3, 20, 71, 23, 8, 1}
+#        songs_data.append((song, array))
