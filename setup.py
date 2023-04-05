f7b2881084e69d044be27b55cf67f98add476b5a
gvanzin-allegheny <vanzing@allegheny.edu>
2023-04-05 19:36:21 -0400

    Add new version tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='musisort',
-    version='0.0.1',    
+    version='1.0.1',    
     description='Automatic Music Categorization Tool',
     url='https://github.com/ReadyResearchers/AutoMusicSort',
     author='Garrison Vanzin',
@@ -22,6 +22,6 @@ setup(
                       ],
 
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 6 - Stage 2',
     ],
 )
